a8bf70ee4c2f244bc69c33ac816fcb98f699f0bc
reka81 <marouanegaming3@gmail.com>
2024-05-28 20:32:57 +0100

    fuah

--- store_ll5.c
+++ store_ll5.c
@@ -6,7 +6,7 @@
 /*   By: mettalbi <mettalbi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:08:24 by zaheddac          #+#    #+#             */
-/*   Updated: 2024/05/27 16:26:54 by mettalbi         ###   ########.fr       */
+/*   Updated: 2024/05/28 20:31:23 by mettalbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	num_herdog(t_stack *lol)
 void	if_next_is_space(t_stack **lst, char **splitting,
 	t_int *lor_int, int *j)
 {
-	if ((*lst)->did_expand == 40 && (*lst)->type != 2)
+	if ((*lst)->did_expand == 40 && (*lst)->type != 1)
 	{
 		splitting = ft_split((*lst)->value, ' ');
 		while (splitting[*j])
@@ -54,25 +54,76 @@ void	if_next_not_space(t_stack **lst, t_int *lor_int, char **splitting, int *j)
 {
 	if((*lst)->next->type != 3)
 	{
-		lor_int->str[lor_int->z] = ft_strjoin((*lst)->value,
-				(*lst)->next->value);
-		if ((*lst)->did_expand == 40 || (*lst)->next->did_expand == 40)
+		// lor_int->str[lor_int->z] = ft_strjoin((*lst)->value,
+		// 		(*lst)->next->value);
+		if ((*lst)->did_expand == 40)
 		{
-			splitting = ft_split((lor_int)->str[lor_int->z], ' ');
-			while (splitting[*j])
+			if ((*lst)->type == 0)
 			{
-				lor_int->str[lor_int->z] = splitting[*j];
-				(*j)++;
+				splitting = ft_split((*lst)->value, ' ');
+				while (splitting[*j])
+				{
+					lor_int->str[lor_int->z] = splitting[*j];
+					(*j)++;
+					lor_int->z++;
+				}
+				j = 0;
+			}
+			else if((*lst)->type == 1)
+			{
+				lor_int->str[lor_int->z] = (*lst)->value;
 				lor_int->z++;
 			}
 		}
 		else
+		{
+			lor_int->str[lor_int->z] = (*lst)->value;
 			lor_int->z++;
+		}
+		if ((*lst)->next->did_expand == 40)
+		{
+			if ((*lst)->next->type == 1)
+			{
+				lor_int->str[lor_int->z - 1] = ft_strjoin(lor_int->str[lor_int->z - 1], (*lst)->next->value);
+			}
+			else if ((*lst)->next->type == 0)
+			{
+				if ((*lst)->next->value[0] != ' ')
+				{
+					splitting = ft_split((*lst)->next->value, ' ');
+					while (splitting[*j])
+					{
+						if ((*j) == 0)
+						{
+							lor_int->str[lor_int->z - 1] = ft_strjoin(lor_int->str[lor_int->z - 1], (*lst)->next->value);
+						}
+						else
+							lor_int->str[lor_int->z] = splitting[*j];
+						(*j)++;
+						lor_int->z++;
+					}
+					j = 0;
+				}
+				else
+				{
+					splitting = ft_split((*lst)->next->value, ' ');
+					while (splitting[*j])
+					{
+						lor_int->str[lor_int->z] = splitting[*j];
+						(*j)++;
+						lor_int->z++;
+					}
+					j = 0;
+				}
+			}
+		}
+		else
+			lor_int->str[lor_int->z - 1] = ft_strjoin(lor_int->str[lor_int->z - 1], (*lst)->next->value);
 		(*lst) = (*lst)->next;
 	}
 	else
 	{
-		if ((*lst)->did_expand == 40)
+		if ((*lst)->did_expand == 40 && (*lst)->type != 1)
 		{
 			splitting = ft_split((*lst)->value , ' ');
 			while (splitting[*j])
@@ -89,6 +140,7 @@ void	if_next_not_space(t_stack **lst, t_int *lor_int, char **splitting, int *j)
 		}
 	}
 }
+
 
 void	if_next_is_null(t_stack **lst, char **splitting, t_int *lor_int, int *j)
 {
